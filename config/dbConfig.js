671fbef6c887180976c29956bbe98fd7f861eaa5
Raul Garcia <raulgarciain@gmail.com>
2024-03-31 15:00:08 -0700

    changed connection pool size for mongodb

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,17 +1,25 @@
-// dbConfig.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 // Load environment variables from .env file
 dotenv.config();
 
 // MongoDB connection URI
 const MONGODB_URI = process.env.MONGODB_URI; // Replace with your MongoDB URI
 
-// Function to establish the database connection
+// Function to establish the database connection with connection pooling
 async function connectDB() {
   try {
-    // Connect to MongoDB
-    await mongoose.connect(MONGODB_URI);
+    // Configure MongoDB connection options with connection pooling
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      poolSize: 10, // Adjust the pool size as needed based on your server capacity
+      // Other options...
+    };
+
+    // Connect to MongoDB using Mongoose with the configured options
+    await mongoose.connect(MONGODB_URI, options);
 
     console.log('Database connected successfully');
   } catch (error) {
